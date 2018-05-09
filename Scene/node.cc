73fe5982b7fa90f8e099cb81f31eb2e9f1804bea
TaiPrev <35993112+TaiPrev@users.noreply.github.com>
2018-05-09 10:35:21 +0200

    Add files via upload

--- Scene/node.cc
+++ Scene/node.cc
@@ -473,7 +473,21 @@ void Node::updateCull(Camera * cam,
 //          update m_isCulled accordingly.
 
 void Node::frustumCull(Camera *cam) {
-	
+	int cut = cam->checkFrustum(m_containerWC);
+	//like this we obtain the frustum check with the Camera-> -1 = isn't in; 0 = cuts with the camera's frustum; 1 = inside the camera's frustum
+	if (cut == -1){
+		this->setCulled(true);	//true, the item IS culled, hence out of the camera's view
+	}
+	else if (cut == +1){
+		this->setCulled(false);	//false
+	}
+	else if (cut == 0){
+		for(list<Node *>::const_iterator it = m_children.begin(), end = m_children.end(); it != end; ++it) {
+			//ITERAMOS por los hijos
+			const Node *theChild = *it;
+			theChild->frustumCull(*cam);
+		}
+	}
 }
 
 // @@ TODO: Check whether a BSphere (in world coordinates) intersects with a
